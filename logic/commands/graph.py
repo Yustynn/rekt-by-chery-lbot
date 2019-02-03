31c36a2bfbf59988d27fd246041297435066181c
Yustynn Panicker <yustynn@gmail.com>
2019-02-03 18:14:42 +0800

    Propagate damage multiplier to graph

--- logic/commands/graph.py
+++ logic/commands/graph.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from ..db.db import get_rekt_records
 from ..misc.helpers import build_people_menu
+from ..misc.config import DAMAGE_MULTIPLIER
 
 GRAPH_PHOTO_PATH = './graph.png'
 GRAPH_LINE_COLORS = [
@@ -29,15 +30,15 @@ def handle_graph_who(bot, update):
 
     records = get_rekt_records(who, fields=['severity'])
     xs = list(records.keys())
-    ys = [sum([r['severity'] for r in rekts]) for rekts in records.values()]
+    ys = [sum([r['severity'] * DAMAGE_MULTIPLIER for r in rekts]) for rekts in records.values()]
 
     fig, ax = plt.subplots()
 
     ax.plot(xs, ys, color=choice(GRAPH_LINE_COLORS), lw=3)
 
-    ax.set_title(f"{who}'s 5-Day Wreckage")
+    ax.set_title(f"{who}'s 5-Day Damage")
     ax.set_xlabel("Date")
-    ax.set_ylabel("Daily Wreckage")
+    ax.set_ylabel("Daily Damage")
 
     fig.savefig(GRAPH_PHOTO_PATH)
     photo = open(GRAPH_PHOTO_PATH, 'rb')
